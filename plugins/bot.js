5384b9ff1444ce682312ea9f67ce89bc1ed25ebb
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-02-09 12:31:57 +0530

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -3,13 +3,13 @@ let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let av = `./Assets/mp3/${pickRandom(["Guru", "Guru1", "Guru2", "Guru3", "Guru4"])}.mp3`
+let av = `./Assets/mp3/${pickRandom(["Bot", "Bot1", "Bot2", "Bot3", "Bot4", "Bot5", "bot6", "Bot7", "Bot8", "Bot9", "Bot10", "Bot11", "Bot12", "Bot13", "Bot14", "Bot15", "Babu"])}.mp3`
 
 m.reply( `Hello ${taguser} Need help?  type /help `)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
-handler.customPrefix = /^(bot|guru)$/i
+handler.customPrefix = /^(bot|samrat|samridhya|saraswati|Priya)$/i
 handler.command = new RegExp
 
 export default handler

9dad42d02fb6af9e4db97eb60445b200d679b6c3
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-02-13 15:15:43 +0530

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -5,7 +5,7 @@ let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let av = `./Assets/mp3/${pickRandom(["Bot", "Bot1", "Bot2", "Bot3", "Bot4", "Bot5", "bot6", "Bot7", "Bot8", "Bot9", "Bot10", "Bot11", "Bot12", "Bot13", "Bot14", "Bot15", "Babu"])}.mp3`
 
-m.reply( `Hello ${taguser} Need help?  type /help `)
+m.reply( `Hello ${taguser} Nice To meet you ❤️`)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 

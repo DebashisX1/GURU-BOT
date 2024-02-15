20df6cb6d441c11e9d6b3e1b26fba4f36d429777
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-02-15 19:14:33 +0530

    Update jaySriRam.js

--- plugins/jaySriRam.js
+++ plugins/jaySriRam.js
@@ -2,7 +2,7 @@ export async function all(m) {
 	
   // when someone sends a group link to the bot's dm
   if ((m.mtype === 'Ram' || m.text.startsWith('Jay') || m.text.startsWith('Jay sri ram')) && !m.isBaileys && !m.isGroup) {
-   this.sendMessage(m.chat,{text:`Jay Jay Sree Ram 🚩;
+   this.sendMessage(m.chat,{text:`Jay Jay Sree Ram 🚩`})
    m.react('🕉️')
 } 
 

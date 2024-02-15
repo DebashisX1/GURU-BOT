b8251124d8fad2b2451f27d48ffcc823df0b13a4
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-02-16 00:34:55 +0530

    Update ram2.js

--- plugins/ram2.js
+++ plugins/ram2.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let av = `./Assets/ram/${pickRandom(["ram", "ram1", "ram2", "ram3", "ram4", "ram5"])}.mp3`
+let av = `./Assets/ram/${pickRandom(["ram", "ram1", "ram2", "ram3", "ram4", "ram5", "ram6", "ram7", "ram8"])}.mp3`
 
 m.reply( `Jay Jay Sree Ram 🚩`)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })

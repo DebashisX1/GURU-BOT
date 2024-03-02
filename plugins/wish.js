c2470b6a8b11dbc434e096a7b02cc4db57fb43f0
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-03-02 11:13:13 +0530

    Update wish.js

--- plugins/wish.js
+++ plugins/wish.js
@@ -1,8 +1,15 @@
-import fs from 'fs'
-import fetch from 'node-fetch'
-m.react('🤠');
+// Assume you have a function to send voice messages called `sendVoiceMessage`
 
-handler.customPrefix = /^(Good Morning)$/i
-handler.command = new RegExp
+// Function to check the message content and send appropriate voice messages
+function handleIncomingMessage(message) {
+    const lowerCaseMessage = message.toLowerCase();
 
-export default handler
+    if (lowerCaseMessage.includes('good morning')) {
+        // Send Good Morning voice message
+        sendVoiceMessage('https://github.com/Samridhya01/GURU-BOT/raw/main/Assets/html/htmla.mp3');
+    }
+}
+
+// Example usage:
+const userMessage = 'Good morning, Bot!';
+handleIncomingMessage(userMessage);

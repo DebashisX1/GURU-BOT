d83f74bf7517026109f1e9341c2063908087f099
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-03-02 12:28:01 +0530

    Update wish.js

--- plugins/wish.js
+++ plugins/wish.js
@@ -5,7 +5,6 @@ let handler = async (m, { conn }) => {
     let av = `./Assets/mp3/Babu.mp3`;
     m.react('🤠');
 
-   
     conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true });
 }
 

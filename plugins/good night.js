bd93b144fb3ea4f77d73d96eac9067b47e156580
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-03-02 17:58:01 +0530

    Update good night.js

--- plugins/good night.js
+++ plugins/good night.js
@@ -1,7 +1,4 @@
 let handler = async (m, { conn }) => {
-    let user = global.db.data.users[m.sender];
-    let name = conn.getName(m.sender);
-    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
     let av = `./Assets/mp3/Bot1.mp3`;
     m.react(🌃);
 
@@ -9,7 +6,7 @@ let handler = async (m, { conn }) => {
     conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true });
 }
 
-handler.customPrefix = /^(Good Night)$/i;
+handler.customPrefix = /^(Night)$/i;
 handler.command = new RegExp;
 
 export default handler;

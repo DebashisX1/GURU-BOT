3a628b987a6a6011d7903c6500038a13c4fb7f9e
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-03-02 11:48:25 +0530

    Update wish.js

--- plugins/wish.js
+++ plugins/wish.js
@@ -1,5 +1,4 @@
 let handler = async (m, { conn }) => {
-    let user = global.db.data.users[m.sender];
     let av = `./Assets/mp3/Babu.mp3`;
     m.react('🤠');
 
@@ -11,17 +10,3 @@ handler.customPrefix = /^(bot)$/i;
 handler.command = new RegExp;
 
 export default handler;
-
-let handler2 = async (m, { conn }) => {
-    let user = global.db.data.users[m.sender];
-    let av = `./Assets/mp3/alive.mp3`;
-    m.react('🤠');
-
-    // Send the audio message without the introductory message
-    conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true });
-}
-
-handler2.customPrefix = /^(bot2)$/i;
-handler2.command = new RegExp;
-
-export default handler2;

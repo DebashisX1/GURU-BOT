b00a50055688232639403ce1f50af1ae2d3d89c2
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-03-02 13:20:41 +0530

    Update wish.js

--- plugins/wish.js
+++ plugins/wish.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn }) => {
-    let user = global.db.data.users[m.sender];
-    let name = conn.getName(m.sender);
-    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
     let av = `./Assets/mp3/Babu.mp3`;
     m.react('🌅');
 
+    // Send the audio message without the introductory message
     conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true });
 }
 
-handler.customPrefix = /^(Good morning)$/i;
-handler.command = new RegExp;
+handler.customPrefix = ['Good morning'];
+handler.command = ['Good morning'];
+
+export default handler;

216c7f80445827c39c5896d93cfd852bd14af8bf
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-03-03 10:22:11 +0530

    Update good night.js

--- plugins/good night.js
+++ plugins/good night.js
@@ -1,8 +1,12 @@
 let handler = async (m, { conn }) => {
-    let av = `./Assets/mp3/Bot1.mp3`;
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender);
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let av = `./Assets/mp3/bot.mp3`;
     m.react(🌃);
 
 
+    
     conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true });
 }
 
@@ -11,3 +15,6 @@ handler.command = new RegExp;
 
 export default handler;
 
+function pickRandom(list) {
+    return list[Math.floor(list.length * Math.random())];
+}

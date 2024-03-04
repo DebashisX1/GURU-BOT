46585f26911bcf315a80c25c6ccf8c317713d077
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-03-04 19:51:07 +0530

    Update good night.js

--- plugins/good night.js
+++ plugins/good night.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
+     m.react(🌃);
     let img = "https://avatars.githubusercontent.com/u/106463398?v=4"
 let info = `*Good Night*`
 await conn.reply(m.chat, info, m, { contextInfo: { mentionedJid: [m.sender],forwardingScore: 256,

7bb35a847f319142f4b668bce27a386f6c5dfe50
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-03-12 20:35:46 +0530

    Update fun-lesbian.js

--- plugins/fun-lesbian.js
+++ plugins/fun-lesbian.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn }) => {
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let name = conn.getName(who)
   let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Guru.jpg')
-  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/overlay/bisexual', {
+  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/bisexual', {
     avatar: pp, 
-  }), 'guru.jpg', `DP of ${name}`, m)
+  }), 'samrat.jpg', `${name} ay chilo tor mona/n rakachilish gopona`, m)
 }
 
 handler.help = ['lesbainpp @user']

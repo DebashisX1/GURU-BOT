646f2a8448bdf4ad43d22a7d098142f51ce2d131
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-03-12 21:24:30 +0530

    Update Fun-jail.js

--- plugins/Fun-jail.js
+++ plugins/Fun-jail.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let name = conn.getName(who)
   let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Guru.jpg')
-  conn.sendFile(global.API('https://some-random-api.com', '/canvas/overlay/jail', {
+  conn.sendFile(global.API('https://some-random-api.com', 'canvas/overlay/jail', {
     avatar: pp, 'samrat.jpg', `partho ar pit chulkaba ${name}`, m)
 }
 

eb2eeff7ae649b767cbdd5502ffc54b6bed52af9
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-03-12 09:56:45 +0530

    Update morning.js

--- plugins/morning.js
+++ plugins/morning.js
@@ -1,5 +1,5 @@
 const handler = async (m, { conn }) => {
-    let av = './Assets/mp3/bot.mp3';
+    let av = './Assets/mp3/bot1.mp3';
     await m.react('🌅');
 
     await conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true });

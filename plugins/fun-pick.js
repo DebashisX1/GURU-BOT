b28f250544a7a395f1fcf995070d7194f31a2525
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-03-26 19:14:27 +0530

    Create fun-pick.js

---
+++ plugins/fun-pick.js
@@ -0,0 +1,19 @@
+let toM = a => '@' + a.split('@')[0];
+function handler(m, { groupMetadata, text }) {
+ let ps = groupMetadata.participants.map(v => v.id);
+    let a = m.sender;
+    let b;
+    do {
+        b = ps[Math.floor(Math.random() * ps.length)]; // Select a random participant
+    } while (b === a);
+m.reply(`The most ${text} is ${toM(b)}`, null, {
+        mentions: [a, b]
+});
+}
+
+handler.help = ['pick'];
+handler.tags = ['fun'];
+handler.command = ['pick'];
+handler.group = true;
+
+export default handler;

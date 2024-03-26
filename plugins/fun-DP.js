396f5e851509d64806d46f2252d40286fe0e72d9
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-03-26 21:55:47 +0530

    Update fun-DP.js

--- plugins/fun-DP.js
+++ plugins/fun-DP.js
@@ -2,9 +2,10 @@ let handler = async (m, { conn }) => {
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let name = conn.getName(who)
   let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Guru.jpg')
-  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/jpg', {
+  conn.sendFile(m.chat, forwardingScore: 256,
+      isForwarded: true, global.API('https://some-random-api.com', '/canvas/misc/jpg', {
     avatar: pp, 
-  }), 'me.png', `I Know ${name} Seen my Photo Everyday At 3:00 AM 💔`, m)
+  }), 'me.png', `@${name} I Seen This Photo Everyday At 3:00 AM 💔`, m)
 }
 
 handler.help = ['upp @user']

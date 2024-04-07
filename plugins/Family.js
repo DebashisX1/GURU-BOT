cba9dc0c230574b5c78b46ee3825ffd175f16ce9
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-04-08 00:19:20 +0530

    Update Family.js

--- plugins/Family.js
+++ plugins/Family.js
@@ -5,13 +5,11 @@ let toM = a => {
 
 function getRandomPercentage() {
     // Generate a random percentage between 1 and 100
-    return Math.floor(Math.random() * 11) + 1;
+    return Math.floor(Math.random() * 12) + 1;
 }
 
 function handler(m, { groupMetadata }) {
     let ps = groupMetadata.participants.map(v => v.id);
-    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  let name = conn.getName(who)
     let a = m.sender;
     if (!a) return; // Defensive programming: exit function if `a` is undefined
     
@@ -34,30 +32,28 @@ function handler(m, { groupMetadata }) {
     // Assign values to e and f
     e = ps[Math.floor(Math.random() * ps.length)];
     f = ps[Math.floor(Math.random() * ps.length)];
-     g = ps[Math.floor(Math.random() * ps.length)];
-    
+    g = ps[Math.floor(Math.random() * ps.length)];
     const percentage = getRandomPercentage();
 
-    m.reply(`গোপন সূত্র থেকে পাওয়া ${name} চৌদ্দগুষ্টির বিবরণ:\n
-বাবা: lord Samridhya 😎\n
+    m.reply(`গোপন সূত্র থেকে পাওয়া ${toM(a)} চৌদ্দগুষ্টির বিবরণ:\n
+বাবা: lord Samridhya 👨\n
 মা/2nd বাবা:‌- ${toM(b)}🫃\n
 ভাই/বোন:- ${toM(c)}💆\n
 বর/বউ:- ${toM(a)}👫\n
 Bf/Gf:- ${toM(a)} 👩‍❤‍💋‍👨\n
+ক্রাশ :- ${toM(g)} \n
 Ex:- ${toM(d)} 🤡\n
-ক্রাশ:- ${toM(g)} 🙈\n
 বাড়িওয়ালা- ${toM(e)} 🏟\n
 শশুর/শাশুরি:- ${toM(f)} 🧚\n
-    Total বাচ্চা: ${percentage}\n\n
-${name} এই হল তোমার আসল পরিচয়, কাল সবাইকে নিয়ে নবান্ন দেখা কর।🤸‍♂`,{ contextInfo: { mentionedJid: [m.sender],forwardingScore: 256,
-      isForwarded: true, externalAdReply: { title: author, body: botname, sourceUrl: fgyt, thumbnail: await conn.getFile(img) }}}, {
+Total Childs: ${percentage}\n\n
+${toM(a)} এই হল তোমার আসল পরিচয় কাল সবাইকে নিয়ে নবান্ন দেখা কর।🤸‍♂`, null, {
         mentions: [a, b, c, d, e, f] // Include all variables in mentions
     });
 }
 
-handler.help = ['family2 @user'];
+handler.help = ['pamily @user'];
 handler.tags = ['fun'];
-handler.command = ['family2'];
+handler.command = ['pamily'];
 handler.group = true;
 
 export default handler;

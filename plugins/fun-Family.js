d88dea9b8414e465ff970c964304f1303a8efcb9
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-04-07 22:57:10 +0530

    Update fun-Family.js

--- plugins/fun-Family.js
+++ plugins/fun-Family.js
@@ -3,53 +3,55 @@ let toM = a => {
     return '@' + a.split('@')[0];
 };
 
-function getRandomPercentage() {
-    // Generate a random percentage between 1 and 100
-    return Math.floor(Math.random() * 100) + 1;
-}
-
-function handler(m, { groupMetadata }) {
+async function handler(m, { conn, groupMetadata }) {
     let ps = groupMetadata.participants.map(v => v.id);
-    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  let name = conn.getName(who)
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe() ? conn.user.jid : m.sender;
+    let name = await conn.getName(who);
     let a = m.sender;
     if (!a) return; // Defensive programming: exit function if `a` is undefined
-    
+
     let b, c, d, e, f; // Declare additional variables
-    
+
     // Select random participants until it's not the same as a
     do {
         b = ps[Math.floor(Math.random() * ps.length)];
     } while (b === a);
-    
+
     // Select additional random participants until they are not the same as a or each other
     do {
         c = ps[Math.floor(Math.random() * ps.length)];
     } while (c === a || c === b);
-    
+
     do {
         d = ps[Math.floor(Math.random() * ps.length)];
     } while (d === a || d === b || d === c);
-    
+
     // Assign values to e and f
     e = ps[Math.floor(Math.random() * ps.length)];
     f = ps[Math.floor(Math.random() * ps.length)];
-    
+
     const percentage = getRandomPercentage();
 
-    m.reply(`গোপন সূত্র থেকে পাওয়া ${name} চৌদ্দগুষ্টির বিবরণ:\n
-বাবা: lord Samridhya 👨\n
-মা/2nd বাবা:‌- ${toM(b)}🫃\n
-ভাই/বোন:- ${toM(c)}💆\n
-বর/বউ:- ${toM(a)}👫\n
-Bf/Gf:- ${toM(a)} 👩‍❤‍💋‍👨\n
-Ex:- ${toM(d)} 🤡\n
-বাড়িওয়ালা- ${toM(e)} 🏟\n
-শশুর/শাশুরি:- ${toM(f)} 🧚\n
-Total Childs: ${percentage}\n
-${name} এই হল তোমার আসল পরিচয় কাল সবাইকে নিয়ে নবান্ন দেখা কর।🤸‍♂`, null, {
+    conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/simpcard', {
+        avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+    }), 'error.png', `গোপন সূত্র থেকে পাওয়া ${name} চৌদ্দগুষ্টির বিবরণ:
+    বাবা: lord Samridhya 👨
+    মা/2nd বাবা:‌- ${toM(b)}🫃
+    ভাই/বোন:- ${toM(c)}💆
+    বর/বউ:- ${toM(a)}👫
+    Bf/Gf:- ${toM(a)} 👩‍❤‍💋‍👨
+    Ex:- ${toM(d)} 🤡
+    বাড়িওয়ালা- ${toM(e)} 🏟
+    শশুর/শাশুরি:- ${toM(f)} 🧚
+    Total Childs: ${percentage}
+    ${name} এই হল তোমার আসল পরিচয় কাল সবাইকে নিয়ে নবান্ন দেখা কর।🤸‍♂`, null, {
         mentions: [a, b, c, d, e, f] // Include all variables in mentions
-    });
+    }, m);
+}
+
+function getRandomPercentage() {
+    // Generate a random percentage between 1 and 100
+    return Math.floor(Math.random() * 12) + 1;
 }
 
 handler.help = ['family @user'];
